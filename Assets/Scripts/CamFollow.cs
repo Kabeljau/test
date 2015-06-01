93f574a0c2a753295ba21eb844c97053c33ac986
Lorena M <lorena-m@web.de>
2015-06-01 11:11:19 +0200

    erstes Castle gebaut

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -9,22 +9,37 @@ public class CamFollow : MonoBehaviour {
 	public GameObject poi; //don't make it public since it might change during runtime(e.g. next projectile)-->change via script
 
 	private float CamZ;
+	private float minCamY;
+	private float leftCam;
+	private Vector2 minXY;
 
 	//awake is always called, even when the attached object is set unactive. start starts as soon as it's set active
 	void Awake(){
 		s = this;
 		CamZ = transform.position.z;
+		minCamY = transform.position.y;
+		leftCam = transform.position.x;
 
 	}
 
-	void Update(){
+	void FixedUpdate(){
 		//check if poi is empty--- if there is no point of interes yet (in our case it would be the projectile)
 		if (poi == null) {return;}
 
+		Vector3 poiTrans = poi.transform.position;
 		Vector3 destination = poi.transform.position;
+
+
+
+		destination = Vector3.Lerp ( transform.position, poiTrans, 0.15f);
 		destination.z = CamZ;
 
+		destination.x = Mathf.Max (minXY.x, destination.x); //prevents camera of jumping under the bottom
+		destination.y = Mathf.Max (minXY.y, destination.y);
+
+
 		transform.position = destination;
+
 	}
 
 }
---
+++ Assets/Scripts/CloudController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+
+	//Fields in the Inspector panel
+	public int numClouds;
+
+	public GameObject[] cloudPrefabs;
+
+	public Vector3 cloudPosMin;
+	public Vector3 cloudPosMax;
+
+	public float speed;
+
+	public float minScale;
+	public float maxScale;
+
+	//Interal Fields
+	public GameObject[] cloudInstances;
+
+
+	void Awake(){
+		//Create an array to hold our cloud instances
+		cloudInstances = new GameObject[numClouds];
+
+		//Find the clouds parent object
+		GameObject parent = GameObject.FindGameObjectWithTag ("CloudParent");
+
+		//Iterate through array and create each cloud
+		GameObject cloud;
+		for (int i=0; i< cloudInstances.Length; i++) {
+
+			//Pick a random cloud prefab between 0 and cloudPrefabs.length - 1
+			int PrefabNum = Random.Range (0, cloudPrefabs.Length - 1);
+		
+
+			//Instantiate a cloud and position it accordingly 	//Scale the cloud
+
+			cloud = Instantiate (cloudPrefabs [PrefabNum]);
+
+			Vector3 cPos = Vector3.zero;
+			cPos.x = Random.Range (cloudPosMin.x, cloudPosMax.x);
+			cPos.y = Random.Range (cloudPosMin.y, cloudPosMax.y);
+			//float scaleFactor = Random.Range (minScale, maxScale);
+
+			//Leissler
+			float scaleU = Random.value;
+			float scaleVal = Mathf.Lerp (minScale, maxScale, scaleU);
+
+			cPos.y = Mathf.Lerp (cloudPosMin.y, cPos.y, scaleU);
+
+			cPos.z = 100 - 90 * scaleU;
+
+			cloud.transform.position = cPos;
+			cloud.transform.localScale = Vector3.one * scaleVal;
+
+			//make the cloud a child of the clouds parent
+			cloud.transform.parent = parent.transform;
+
+			//Add the cloud to our cloudInstances
+			cloudInstances [i] = cloud;
+		}
+		Debug.Log (cloudInstances.Length);
+	}
+
+		void Update(){
+
+			//Iterate over all cloud objects in the background
+			foreach(GameObject cloud in cloudInstances){
+				// get scale and position of each cloud
+			float scaleVal = cloud.transform.localScale.x;
+			Vector3 cPos = cloud.transform.position;
+
+				//Move larger clouds faster
+				cPos.x -= Time.deltaTime * speed * scaleVal;
+
+			if(cPos.x < cloudPosMin.x){
+				cPos.x = cloudPosMax.x;
+			}
+			cloud.transform.position = cPos;
+			}
+		}
+}
+
+
+
+
+
+
